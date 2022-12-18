12f912a9eeb4dac01d15118bdb83175dbfa71cd1
insafcherif <cherifinsaf.inat@gmail.com>
2022-12-18 23:03:58 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-        <h1>Hello Dogo!!</h1>
-        <h2>Things I need to do</h2>
-        <li>Lear react</li>
-        <li>Climb Mt Everst</li>
+      <h1>Hello Dogo!!</h1>
+      <h2>Things I need to do</h2>
+      <ul>
+        <li>Learn React</li>
+        <li>Climb Mt. Everest</li>
+        <li>Run a marathon</li>
+        <li>Feed the dog</li>
+        <li>Get rid of .App slector in App.css</li>
+      </ul>
     </div>
   );
 }
